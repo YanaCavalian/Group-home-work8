8a197cce1f9e76f79316a720134d19253c8793da
Yana <141483662+YanaCavalian@users.noreply.github.com>
2023-08-10 18:55:51 +0100

    Revert "Added slider"

--- javascript.js
+++ javascript.js
@@ -2,30 +2,3 @@
 span = document.querySelector('span');
 span.style.color="blue";
 setTimeout(() => { document.querySelector('span').innerHTML = "This site is the best";}, 4000);
-
-const slider = document.querySelector('.slider');
-const sliderInner = document.querySelector('.slider-inner');
-let currentSlide = 0;
-
-function nextSlide() {
-    currentSlide++;
-    if (currentSlide >= sliderInner.children.length) {
-        currentSlide = 0;
-    }
-    updateSlider();
-}
-
-function prevSlide() {
-    currentSlide--;
-    if (currentSlide < 0) {
-        currentSlide = sliderInner.children.length - 1;
-    }
-    updateSlider();
-}
-
-function updateSlider() {
-    const offset = currentSlide * slider.offsetWidth;
-    sliderInner.style.transform = `translateX(-${offset}px)`;
-}
-
-setInterval(nextSlide, 4000);
